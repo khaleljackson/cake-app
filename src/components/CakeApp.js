029eeffa701c2f244a439ddfa95103730d54728d
Khaleljackson <khaleljackson@gmail.com>
2020-07-03 01:41:54 +0200

    2nd version

--- src/components/CakeApp.js
+++ src/components/CakeApp.js
@@ -5,12 +5,14 @@ import {Col} from 'react-bootstrap';
 export class CakeApp extends Component {
     render() {
         return (
-            <Col lg="12">
-                the main container
+            <Col lg="12" style={mainStyle}>
                 <CakeList cakes={this.props.cakes} delCake={this.props.delCake} editCake={this.props.editCake}/>
             </Col>
         )
     }
+}
+const mainStyle={
+    backgroundColor:'#f4f4f4'
 }
 
 export default CakeApp
--- src/components/CakeList.js
+++ src/components/CakeList.js
@@ -7,7 +7,7 @@ export class CakeList extends Component {
     render() {
         return (
             <Col md="12" lg="12">
-                <p>Here is the list body</p>
+                <h4>Yummy Cake list Below:</h4>
                 <Row>
                     <ListItem cakes={this.props.cakes} delCake={this.props.delCake} editCake={this.props.editCake}/>
                 </Row>
